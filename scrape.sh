5d8f81b8ceff1aba46247687cb7ad86f051577fc
kenellorando <kenellorando@gmail.com>
2018-10-02 14:42:25 -0500

    Grab plaintext of each doc/docx file

--- scrape.sh
+++ scrape.sh
@@ -8,10 +8,11 @@ then
     exit 1
 fi
 
+# Find all .doc/.docx files.
+find $1 -type f -name "*.doc" -o -name "*.docx" | while read file; do
+    echo $file
+    # Extract the plaintext of the document
+    unzip -p $file word/document.xml | sed -e 's/<\/w:p>/\n/g; s/<[^>]\{1,\}>//g; s/[^[:print:]\n]\{1,\}//g'
+done
 
-
-
-# Recursive walk on directory.
-# Extract plain text from docx file.
-    # Use: unzip -p some.docx word/document.xml | sed -e 's/<[^>]\{1,\}>//g; s/[^[:print:]]\{1,\}//g'
 # Filter IOCs using various regexs.
