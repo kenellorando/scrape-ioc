2856e01d6b77e15e27cb86f8cbe81d770dbe3916
kenellorando <kenellorando@gmail.com>
2018-10-18 12:55:04 -0500

    Regex match IP, email, MD5, sha356

--- scrape.sh
+++ scrape.sh
@@ -10,9 +10,23 @@ fi
 
 # Find all .doc/.docx files.
 find $1 -type f -name "*.doc" -o -name "*.docx" | while read file; do
-    echo $file
-    # Extract the plaintext of the document
-    unzip -p $file word/document.xml | sed -e 's/<\/w:p>/\n/g; s/<[^>]\{1,\}>//g; s/[^[:print:]\n]\{1,\}//g'
+    # Extract the plaintext of the document out of xml
+    plaintext=$(unzip -p $file word/document.xml | sed -e 's/<\/w:p>/\n/g; s/<[^>]\{1,\}>//g; s/[^[:print:]\n]\{1,\}//g')
+
+    # Match valid IP addresses
+    echo $plaintext | grep -E -o "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)"
+
+    # Match valid email addresses
+    echo $plaintext | grep -E -o "\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,6}\b"
+
+    # Match MD5 hashes
+    echo $plaintext | grep -o -w "[0-9a-f]\{32\}"
+
+    # Match SHA 256 hashes
+    echo $plaintext | grep -o -w "[0-9a-f]\{64\}"
 done
 
-# Filter IOCs using various regexs.
+
+
+
+
